7e2025b60dddb82dd84b7c22a8488d918659fac0
MFBarker <98473836+MFBarker@users.noreply.github.com>
2022-10-06 16:32:20 -0600

    OpenGL Setup, Basic Shaders Implementation

--- Application/Application/Main.cpp
+++ Application/Application/Main.cpp
@@ -1,7 +1,27 @@
 #include "Engine.h" 
 #include <iostream> 
 
-int main()
+float points[] = {
+  -0.5f, -0.5f,  0.0f,
+   0.0f,  0.5f,  0.0f,
+   0.5f, -0.5f,  0.0f
+};
+
+const char* vertex_shader =
+"#version 430 core\n"
+"in vec3 position;"
+"void main() {"
+"  gl_Position = vec4(position * 1.2, 1.0);"
+"}";
+
+const char* fragment_shader =
+"#version 430 core\n"
+"out vec4 color;"
+"void main() {"
+"  color = vec4(1.0, 0.0, 0.0, 1.0);"
+"}";
+
+int main(int argc, char** argv)
 {
 	neu::InitializeMemory();
 
@@ -9,6 +29,35 @@ int main()
 	neu::Engine::Instance().Register();
 
 	neu::g_renderer.CreateWindow("Neumont", 800, 600);
+
+	// create vertex buffer
+	GLuint vbo = 0;
+	glGenBuffers(1, &vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER, 9 * sizeof(float), points, GL_STATIC_DRAW);
+
+	// create vertex array
+	GLuint vao = 0;
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+	glEnableVertexAttribArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+
+	// create shader
+	GLuint vs = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vs, 1, &vertex_shader, NULL);
+	glCompileShader(vs);
+	GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fs, 1, &fragment_shader, NULL);
+	glCompileShader(fs);
+
+	// create program
+	GLuint program = glCreateProgram();
+	glAttachShader(program, fs);
+	glAttachShader(program, vs);
+	glLinkProgram(program);
+	glUseProgram(program);
 
 	bool quit = false;
 	while (!quit)
@@ -18,8 +67,13 @@ int main()
 		if (neu::g_inputSystem.GetKeyState(neu::key_escape) == neu::InputSystem::KeyState::Pressed) quit = true;
 
 		neu::g_renderer.BeginFrame();
+
+		glDrawArrays(GL_TRIANGLES,0,3);
+
 		neu::g_renderer.EndFrame();
 	}
 
 	neu::Engine::Instance().Shutdown();
+
+	return 0;
 }
